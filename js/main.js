19f641c803dba850c98ed1e507a7847fca7d63d1
MITOLO Duck <duckmitolo@gmail.com>
2026-09-15 17:47:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -106,16 +106,3 @@ async function init() {
   initActiveNavLink();
   scrollToHashTarget();
 }
-
-const btnEcoles = document.getElementById('btn-tab-ecoles');
-    const btnLogements = document.getElementById('btn-tab-logements');
-    if (btnEcoles && btnLogements) {
-      btnEcoles.addEventListener('click', () => {
-        btnEcoles.classList.add('active');
-        btnLogements.classList.remove('active');
-      });
-      btnLogements.addEventListener('click', () => {
-        btnLogements.classList.add('active');
-        btnEcoles.classList.remove('active');
-      });
-    }
